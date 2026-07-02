6ae387581e8285467d61b042345a1ef1b65c1de7
__Hitesh__ <hiteshpant50@gmail.com>
2026-07-02 23:55:59 +0545

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,13 @@ function App() {
 
   useEffect(() => {
     socket.current = connectWs();
-    
+
+    socket.current.on("connect", () => {
+
+      socket.current.emit('joinRoom', userName);
+
+
+    })
   },[])
 
 
@@ -41,9 +47,35 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-zinc-100 p-4 font-thin">
-        // Enter your name to start chatting
-        {}
+      <div className="min-h-screen flex items-center justify-center bg-zinc-100 p-4 font-inter">
+        
+        {showNamePopup && (
+          <div className="fixed inset-0 flex items-center justify-center z-40">
+            <div className="bg-white rounded-xl shadow-lg max-w-md p-6">
+              <h1 className="text-xl font-semibold text-black">Enter Your name</h1>
+              <p className="text-sm text-gray-500 mt-1">Enter Your nane to start chatting. This will be used to identify you in the chat.</p>
+
+              <form action="" className="mt-4" onSubmit={handleNameSubmit}>
+                <input
+                  autoFocus
+                  value={inputName}
+                  onChange={(e) => setInputName(e.target.value)}
+                  className="w-full border-gray-200 rounded-md px-3 py-2 outline-green-500 placeholder-gray-400"
+                  placeholder="Your name(e.g. Ram)"
+
+                />
+
+
+                <button
+                  type="submit"
+                  className="block ml-auto mt-3 px-4 py-1.5 rounded-full bg-green-500 text-white font-medium cursor-pointer">
+                  Continue
+                </button>
+              </form>
+            </div>
+
+          </div>
+        )}
       </div>
     </>
   )
