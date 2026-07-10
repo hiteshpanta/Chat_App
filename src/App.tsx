acecfdaa0a538cd860da70e694218c55341ff3ca
__Hitesh__ <hiteshpant50@gmail.com>
2026-07-10 22:39:23 +0545

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,16 @@ function App() {
 
     socket.current.on("connect", () => {
 
+      socket.current.on('roomNotice', (userName: string) => {
+        console.log(`${userName} joined to group`);
+      });
+
+      socket.current.on('chatMessage', (msg: string) => {
+
+        // push to existing messages list
+        console.log("mesage: ", msg)
+        setMessages((prev: any) => [...prev, msg])
+      })
       
 
 
@@ -51,8 +61,30 @@ function App() {
   }
 
   function sendMessage() {
-    const t= text.trim();
+    const t: string = text.trim();
     if (!t) return;
+
+    // USER MESSAGE
+    const msg = {
+      id: Date.now(),
+      sender: userName,
+      text: t,
+      ts: Date.now(),
+    };
+
+    setMessages((m: any) => [...m, msg]);
+
+    //emit
+    socket.current('chatMessage', msg);
+
+    setText('');
+  }
+
+  function handleKeyDown(e: any) {
+    if (e.key === 'Enter' && !e.shiftKey){
+      e.preventDefault();
+      sendMessage();
+    }
   }
  
 
@@ -85,6 +117,92 @@ function App() {
               </form>
             </div>
 
+          </div>
+        )}
+
+
+        {/* Chat window*/}
+
+        {!showNamePopup && (
+          <div className="w-full max-w-2xl h-[90vh] bg-white rounded-xl shadow flex flex-col overflow-hidden">
+            <div className="flex items-center gap-3 px-4 py-3 border-b border-gray-200">
+              <div className="h-10 w-10 rounded-full bg-[#075E54] flex items-center justify-center text-white font-semibold">
+                R
+
+              </div>
+
+              <div className="flex-1">
+                <div className="text-sm font-medium text-[#303030]">
+                  Realtime group chat
+                </div>
+                <div className="text-xs text-gray-500">Someone is typing...</div>
+
+              </div>
+
+              <div>
+                Signed in as {' '}
+                <span className="font-medium text-[#303030] capitalize">{userName}</span>
+              </div>
+
+            </div>
+
+
+            {/* CHAT MESSAGE LIST */}
+            <div className="flex-1 overflow-y-auto p-4 space-y-3 bg-zinc-100 flex flex-col">
+              {messages.map((m: any) => {
+                const mine= m.sender === userName;
+
+                return (
+                  <div
+                    key={m.id}
+                    className={`flex ${mine ? 'justify-end' : 'justify-start'}`}
+                  >
+                    <div className={`max-w-[78%] p-3 my-2 rounded-[18px] text-sm leading-5 shadow-sm ${mine ? 'bg-[#DCF8C6 text-[#303030] rounded-br-2xl'
+                      : 'bg-white text-[#303030] rounded-bl-2xl'
+                    }`}>
+                      <div className="break-words whitespace-pre-wrap">
+                        {m.text}
+                      </div>
+
+                      <div className="flex justify-between items-center mt-1 gap-16">
+                        <div className="text-[11px] font-bold">
+                          {m.sender}
+                        </div>
+                        <div className="text-[11px] text-gray-500 text-right">
+                          {formatTime(m.ts)}
+                        </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+                );
+              })}
+
+            </div>
+
+            {/* CHAT TEXTAREA*/}
+
+            <div className="px-4 py-3 border-t border-gray-200 bg-white">
+              <div className="flex items-center justify-between gap-4 border border-gray-200 rounded-full">
+                <textarea name="" id=""
+                  rows={1}
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                  placeholder="Type a message..."
+                  className="w-full resize-none px-4 text-sm outline-none"
+                />
+                <button
+                  onClick={sendMessage}
+                  className="bg-green-500 text-white px-4 py-2 mr-2 rounded-full text-sm font-medium cursor-pointer"
+                >
+                  Send
+                </button>
+
+              </div>
+
+            </div>
           </div>
         )}
       </div>
